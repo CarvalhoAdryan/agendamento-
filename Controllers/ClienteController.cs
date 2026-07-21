3563edfbeddf4cfc31a9fd64b600194814fed768
Adryan Carvalho <adryansc.18@gmail.com>
2026-07-21 12:11:16 -0300

    metodo get

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,8 +1,23 @@
 using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DFsite;
 
-public class ClienteController
+[ApiController]
+[Route("api/[controller]")]
+public class ClienteController : ControllerBase
 {
+    private readonly ClienteService _service;
 
+    public ClienteController(ClienteService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ListarTodos()
+    {
+        var clientes = await _service.ListarTodosAsync(); 
+        return Ok(clientes);
+    }
 }
